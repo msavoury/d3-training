30c5751a21400e34b6889e0aba1dd26fc03206d9
Marcos Savoury <msavoury@Marcoss-MacBook-Pro.local>
2016-11-14 11:30:56 -0500

    stuff

--- loading-data/assets/main.js
+++ loading-data/assets/main.js
@@ -1,7 +1,35 @@
 function dataLoaded(data) {
     console.log(data.name);
+    var name = data.name;
+    var nums = data.nums;
+    var min = d3.min(nums);
+    var max = d3.max(nums);
+    var linearScale = d3.scale.linear().domain([min, max]).range([0, 550]);
+    console.log(max);
+    d3.select('#main').append('svg').attr('height', '600px').attr('width', '600px');
+
+    nums.forEach(function(value, idx) {
+        d3.select("svg")
+            .append("text")
+            .attr("id", "a")
+            .attr("x", linearScale(value))
+            .attr("y",20)
+            .style("opacity", 1)
+            .text("" + value);
+    })
+}
+
+function populationLoaded(dataset) {
+    d3.select("body").selectAll('div.pop')
+        .data(dataset).enter().append('div').attr('class', 'population')
+        .html(function(data, index) {
+            return "Age: " + data.age + " Population: " + data.females;
+        }); 
 }
 
 d3.json('/data.json', function(data) {
    dataLoaded(data);
+});
+d3.json('/population.json', function(data) {
+    populationLoaded(data);
 });
